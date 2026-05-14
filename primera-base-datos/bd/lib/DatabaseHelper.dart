6547d83ac73b10e1c15faa046133e641f0328a93
Lautaro Tamborindeguy <lautarotegui4@gmail.com>
2026-05-13 21:00:00 -0300

    Creating first mobile app with BD

---
+++ primera-base-datos/bd/lib/DatabaseHelper.dart
@@ -0,0 +1,35 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+
+class Databasehelper {
+  static final Databasehelper _instance = Databasehelper._internal();
+  Databasehelper._internal();
+
+  factory Databasehelper() {
+    return _instance;
+  }
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String dbpath = await getDatabasesPath();
+    return openDatabase(
+      join(dbpath, 'cadastro.db'),
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute(
+      'CREATE IF NOT EXISTS TABLE estudiantes (id INTEGER PRIMARY KEY AUTOINCREMENT,nombre TEXT NOT NULL,matricula TEXT NOT NULL)'
+    );
+  }
+}
